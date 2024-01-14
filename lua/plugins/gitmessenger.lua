fd65d92285408ae498a38cff75d12777cc5e1547
Romeu Vieira <romeu.bizz@gmail.com>
2024-01-14 01:06:37 +0000

    Added gitmessenger and diffview

---
+++ lua/plugins/gitmessenger.lua
@@ -0,0 +1,6 @@
+-- Gitmessenger config
+
+vim.g.git_messenger_floating_win_opts = { border = "rounded" }
+vim.g.git_messenger_always_into_popup = 0
+vim.g.git_messenger_extra_blame_args = "-w"
+vim.g.git_messenger_popup_content_margins = false
